421b880059819760822db1e6700ec9d6e19d674e
Nikita Tsukanov <keks9n@gmail.com>
2015-07-15 02:03:58 +0300

    Initial

---
+++ WpfPointEditorExample/DesignData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace WpfPointEditorExample
+{
+	class DesignData
+	{
+		public MainWindowViewModel MainWindowViewModel
+		{
+			get
+			{
+				return new MainWindowViewModel()
+				{
+					Points = new ObservableCollection<EditorPoint>()
+					{
+						new EditorPoint() {X = 50, Y = 70},
+						new EditorPoint() {X = 120, Y = 130},
+						new EditorPoint() {X = 70, Y = 102}
+					}
+				};
+			}
+		}
+	}
+}
---
+++ WpfPointEditorExample/MainWindowViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using JetBrains.Annotations;
+
+namespace WpfPointEditorExample
+{
+	public class MainWindowViewModel : INotifyPropertyChanged
+	{
+		public ObservableCollection<EditorPoint> Points { get; set; }
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		[NotifyPropertyChangedInvocator]
+		protected virtual void OnPropertyChanged(string propertyName)
+		{
+			var handler = PropertyChanged;
+			if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+
+	public class EditorPoint : INotifyPropertyChanged
+	{
+		private int _x;
+		private int _y;
+
+		public int X
+		{
+			get { return _x; }
+			set
+			{
+				if (value == _x) return;
+				_x = value;
+				OnPropertyChanged("X");
+			}
+		}
+
+		public int Y
+		{
+			get { return _y; }
+			set
+			{
+				if (value == _y) return;
+				_y = value;
+				OnPropertyChanged("Y");
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		[NotifyPropertyChangedInvocator]
+		protected virtual void OnPropertyChanged(string propertyName)
+		{
+			var handler = PropertyChanged;
+			if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
